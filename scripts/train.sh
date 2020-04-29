20df9a4890f6c6d93242738cd4599e2ea56d4ad0
vladimir-chernykh <vladimir.chernykh@phystech.edu>
2020-04-29 12:58:30 +0300

    training script

---
+++ scripts/train.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+cd stargan || exit
+
+if [ ! -d "./data/celeba" ]; then
+  chmod +x download.sh
+  ./download.sh celeba
+fi
+
+nohup python main.py \
+     --mode train \
+     --dataset CelebA \
+     --image_size 128 \
+     --c_dim 9 \
+     --sample_dir stargan_celeba/samples \
+     --log_dir stargan_celeba/logs \
+     --model_save_dir stargan_celeba/models \
+     --result_dir stargan_celeba/results \
+     --selected_attrs Black_Hair Blond_Hair Brown_Hair Gray_Hair Bald Bangs Receding_Hairline Straight_Hair Wavy_Hair \
+     &
