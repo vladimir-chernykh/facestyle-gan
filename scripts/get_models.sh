1c18a968a95995ea67607d23df954d9273d2bcea
vladimir-chernykh <vladimir.chernykh@phystech.edu>
2020-04-30 09:46:40 +0300

    add custom models download to get_models script

--- scripts/get_models.sh
+++ scripts/get_models.sh
@@ -9,3 +9,11 @@ mkdir -p "$MODEL_DIR"
 wget -N "$URL" -O "$ZIP_FILE"
 unzip "$ZIP_FILE" -d "$MODEL_DIR"
 rm "$ZIP_FILE"
+
+MODEL_DIR=$DIR/../models/celeba_128_custom
+ZIP_FILE=$MODEL_DIR/model.zip
+URL="https://www.dropbox.com/s/prlyvysidntkyk4/celeba-128x128-9attrs.zip?dl=0"
+mkdir -p "$MODEL_DIR"
+wget -N "$URL" -O "$ZIP_FILE"
+unzip "$ZIP_FILE" -d "$MODEL_DIR"
+rm "$ZIP_FILE"
