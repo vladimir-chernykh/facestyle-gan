6af422caae724216e3a47973dbe0b93438297475
vladimir-chernykh <vladimir.chernykh@phystech.edu>
2020-04-30 17:17:00 +0300

    remove original models download

--- scripts/get_models.sh
+++ scripts/get_models.sh
@@ -2,14 +2,6 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-MODEL_DIR=$DIR/../models/celeba_128_original
-ZIP_FILE=$MODEL_DIR/model.zip
-URL="https://www.dropbox.com/s/7e966qq0nlxwte4/celeba-128x128-5attrs.zip?dl=0"
-mkdir -p "$MODEL_DIR"
-wget -N "$URL" -O "$ZIP_FILE"
-unzip "$ZIP_FILE" -d "$MODEL_DIR"
-rm "$ZIP_FILE"
-
 MODEL_DIR=$DIR/../models/celeba_128_custom
 ZIP_FILE=$MODEL_DIR/model.zip
 URL="https://www.dropbox.com/s/prlyvysidntkyk4/celeba-128x128-9attrs.zip?dl=0"
